7252e36d76ca74d7bef3c9f0285886fb58875b8a
mr.sherbs <102490963+mrsherbs@users.noreply.github.com>
2022-08-23 22:52:28 -0400

    Update background-replacer.py

--- background-replacer.py
+++ background-replacer.py
@@ -14,6 +14,7 @@ if add_logo:
     logo = cv2.resize(logo_image, (100, 100))
 background = functions.convert_list(config.get("background-replacer", "background_color").split(","), int)
 
+# Take a template image and remove the original posters background, to erase any watermarks.
 
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
